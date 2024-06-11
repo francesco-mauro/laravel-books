bee0d8cc5955fb01e98f89a2821f4cc9343387a3
francesco-mauro <francesco.mauro180@gmail.com>
2024-06-11 10:34:44 +0200

    add: corrections

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
+use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\MovieController;
 
 /*
 |--------------------------------------------------------------------------
